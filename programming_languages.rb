f0d93381ead521a6823a4a12f1c36e8babe84ef9
github username <github email address>
2015-08-25 14:00:36 -0400

    dunzo

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
+require "pry"
 def reformat_languages(languages)
   # your code here
+  hash = {}
+  languages.each do |key, value|
+    value.each do |k,v|
+
+      if hash[k]
+        hash[k][:style] << key
+      else
+        v[:style] = [key]
+        hash[k] = v
+      end
+    end
+  end
+  return hash
 end
